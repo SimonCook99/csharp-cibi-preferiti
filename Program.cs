365982e504cf4daa7b21ac1aa3b021650a161100
Simone Mauro <simon.mauro@virgilio.it>
2022-06-23 15:06:49 +0200

    Migliorata concatenazione elementi

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿string[] cibiPreferiti = { "pasta al sugo", "sushi", "pasta al burro", "pizza", "carbonara"};
 
-Console.WriteLine("La lista contiene " + cibiPreferiti.Length + " elementi");
+Console.WriteLine($"La lista contiene {cibiPreferiti.Length} elementi");
 
 for(int i = 0; i < cibiPreferiti.Length; i++){
-    Console.WriteLine("Posizione n° " + i + ": " + cibiPreferiti[i]);
+    Console.WriteLine($"Posizione n° {i} : {cibiPreferiti[i]}"  );
 }
 
 
-Console.WriteLine("Il cibo migliore è: " + cibiPreferiti[cibiPreferiti.Length - 1]);
+Console.WriteLine($"Il cibo migliore è: {cibiPreferiti[cibiPreferiti.Length - 1]} ");
 
-Console.WriteLine("Il cibo peggiore è: " + cibiPreferiti[0]);
+Console.WriteLine($"Il cibo peggiore è: {cibiPreferiti[0]}");
