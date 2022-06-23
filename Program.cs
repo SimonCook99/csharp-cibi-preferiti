ee8c7880b1395b4c82d65857156b4cdc2062dc35
Simone Mauro <simon.mauro@virgilio.it>
2022-06-23 15:21:00 +0200

    bonus completato

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿string[] cibiPreferiti = { "pasta al sugo", "sushi", "pasta al burro", "pizza", "carbonara"};
+﻿string[] cibiPreferiti = { "pasta al sugo", "sushi", "pasta al burro", "pizza"};
 
 Console.WriteLine($"La lista contiene {cibiPreferiti.Length} elementi");
 
@@ -10,3 +10,14 @@ for(int i = 0; i < cibiPreferiti.Length; i++){
 Console.WriteLine($"Il cibo migliore è: {cibiPreferiti[cibiPreferiti.Length - 1]} ");
 
 Console.WriteLine($"Il cibo peggiore è: {cibiPreferiti[0]}");
+
+
+if(cibiPreferiti.Length %2 != 0){ //se la lunghezza dell'array è dispari...
+    //Console.WriteLine(cibiPreferiti.Length / 2);
+    string elementoMediano = cibiPreferiti[cibiPreferiti.Length / 2];
+    Console.WriteLine(elementoMediano);
+}
+else{ // se la lunghezza è pari...
+    Console.WriteLine(cibiPreferiti[cibiPreferiti.Length / 2 - 1]);
+    Console.WriteLine(cibiPreferiti[(cibiPreferiti.Length / 2)]);
+}
