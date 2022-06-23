e10023ca8d8e67c9655ac0a41fadb0511e10fd95
Simone Mauro <simon.mauro@virgilio.it>
2022-06-23 14:55:12 +0200

    Esercizio completato

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] cibiPreferiti = { "pasta al sugo", "sushi", "pasta al burro", "pizza", "carbonara"};
+
+Console.WriteLine("La lista contiene " + cibiPreferiti.Length + " elementi");
+
+for(int i = 0; i < cibiPreferiti.Length; i++){
+    Console.WriteLine("Posizione n° " + i + ": " + cibiPreferiti[i]);
+}
+
+
+Console.WriteLine("Il cibo migliore è: " + cibiPreferiti[cibiPreferiti.Length - 1]);
+
+Console.WriteLine("Il cibo peggiore è: " + cibiPreferiti[0]);
